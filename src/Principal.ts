7df17e19cbf2b4e590dff4f81842383d1008e83d
FranciscoRos <l21390377@chetumal.tecnm.mx>
2025-03-08 21:59:08 -0500

    vamos bien #sarna

--- src/Principal.ts
+++ src/Principal.ts
@@ -1,17 +1,18 @@
+import * as readlineSync from 'readline-sync';
 import { JuegoDados } from "./JuegoDados";
 
 function main(): void {
-  const nombreJugador1 = prompt("Ingrese el nombre del primer jugador:");
-  const nombreJugador2 = prompt("Ingrese el nombre del segundo jugador:");
+  const nombreJugador1 = readlineSync.question("Ingrese el nombre del primer jugador: ");
+  const nombreJugador2 = readlineSync.question("Ingrese el nombre del segundo jugador: ");
 
   if (!nombreJugador1 || !nombreJugador2) {
     console.log("Se requieren los nombres de los jugadores");
     return;
   }
 
-  console.log("Primer jugador: " + nombreJugador1 + "\n");
+  console.log("\nPrimer jugador: " + nombreJugador1);
   console.log("Segundo jugador: " + nombreJugador2);
-  
+
   var juego: JuegoDados = new JuegoDados(nombreJugador1, nombreJugador2);
   juego.iniciarJuego();
 
